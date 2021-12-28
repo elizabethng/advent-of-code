30217715d13b00471a1cba625f5e2b7e2b4b7f85
Elizabeth Ng <elng@uw.edu>
2021-12-27 23:31:29 -0800

    Do day7 part 2

--- day7.R
+++ day7.R
@@ -21,4 +21,37 @@ if(FALSE){
 }
 
 dat <- scan("day7_dat.txt", sep = ",")
-res <- move_crabs(dat) # 345,197 fuel
+res <- move_crabs(dat) # 345197 fuel
+
+
+# Part 2
+move_crabs2 <- function(vec){
+  tmp <- expand_grid(
+    dat = list(vec),
+    position = min(vec):max(vec)
+  ) %>%
+    rowid_to_column("id") %>%
+    unnest(dat) %>%
+    mutate(moves = abs(dat - position)) %>%
+    mutate(cost = map_dbl(moves, ~ sum(1:.x))) %>%
+    mutate(cost = ifelse(moves == 0, cost - 1, cost))
+  # correction for 0:1
+  
+  res <- tmp %>%
+    group_by(id) %>%
+    summarize(
+      cost = sum(cost),
+      .groups = "drop")
+  
+  out <- slice_min(res, order_by = cost)
+  
+  return(out)
+}
+
+if(FALSE){
+  test <- c(16,1,2,0,4,2,7,1,2,14)
+  move_crabs2(test) # 168
+}
+
+res2 <- move_crabs2(dat) # 96361608 # too high!
+                         # 96361606 # correct!
