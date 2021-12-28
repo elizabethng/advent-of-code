e4b4980196aba22908f80f052e92c72d24f1d753
Elizabeth Ng <elng@uw.edu>
2021-12-27 22:49:09 -0800

    Back to array approach for day5

--- day5/day5.R
+++ day5/day5.R
@@ -8,6 +8,7 @@ library("sf")
 
 
 #  Part 1 --------------------------------------------------------------
+# Consider only the horizontal and vertical lines
 # At how many points do at least two lines overlap?
 # i.e., how many intersections
 
@@ -29,9 +30,12 @@ proc_dat <- function(raw){
         dx == 0 & abs(dy) > 0 ~ "vertical",
         TRUE                  ~ "other"
       )
-    ) 
+    ) %>%
+    filter(slope != "other") %>%
+    mutate(d = max(x_from, x_to, y_from, y_to))
   return(out)
 }
+
 make_spatial <- function(dat){
   out <- dat %>%
     select(-dx, -dy) %>%
@@ -67,22 +71,66 @@ get_overlaps <- function(spat){
   return(out)
 }
 
+draw_path <- function(x_from, x_to, y_from, y_to, d){
+  # reindex
+  x_from <- x_from + 1
+  x_to <- x_to + 1
+  y_from <- y_from + 1
+  y_to <- y_to + 1
+  d <- d +1
+  
+  m <- matrix(0, nrow = d, ncol = d)
+  
+  if(x_from == x_to){
+    for(y in y_from:y_to){
+      m[x_to, y] <- 1
+    }
+  } else if(y_from == y_to){
+    for(x in x_from:x_to){
+      m[x, y_to] <- 1
+    }
+  }
+  
+  return(m)
+}
+
+count_cells <- function(dat){
+  tmp <- mutate(dat, 
+                m = pmap(list(x_from, x_to, y_from, y_to, d), 
+                         draw_path))
+  
+  res <- tmp$m[[1]]
+  for(i in 2:nrow(tmp)){
+    res <- res + tmp$m[[i]]
+  }
+  
+  # Get more than 2 overlap cells
+  out <- sum(res > 1)
+  
+  return(out)
+}
+
+
+
+
+
+# Old ---------------------------------------------------------------------
 # Test
 if(FALSE){
   test <- read_lines(here::here("day5", "test.txt")) 
   testdat <- proc_dat(test)
-  ggplot(testdat, aes(group = step, color = slope)) +
+  ggplot(testdat, aes(group = step, color = as.factor(step))) +
     geom_segment(aes(x = x_from, xend = x_to,
                      y = y_from, yend = y_to))
   testspat <- make_spatial(testdat)
   plot(testspat)
-  get_overlaps(testspat) # 9
+  get_overlaps(testspat) # 2 should be 5
+  
+  test %>%
+    proc_dat() %>%
+    count_cells() # 5
 }
 
 read_lines(here::here("day5", "input.txt")) %>%
   proc_dat() %>%
-  # ggplot(aes(group = step, color = slope)) +
-  # geom_segment(aes(x = x_from, xend = x_to,
-  #                  y = y_from, yend = y_to))
-  make_spatial() %>%
-  get_overlaps() # 15,416 
+  count_cells() # 5169
